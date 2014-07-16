7d559097f0052453d783836a94c391f5d783b7c1
Adrien Pétremann <petremann.adrien@gmail.com>
2014-07-16 21:41:59 +0200

    Thieves go to the chest and rotate correctly

--- main.lua
+++ main.lua
@@ -11,29 +11,39 @@ ennemy_id = 1               -- Incremential id of ennemies
 apparation_cooldown = 5    -- Seconds before an ennemy appears
 time_ennemy_pop = nil
 
+-----------------------
+-- Game initialization
+-----------------------
 function love.load()
     love.graphics.setColor( 255, 255, 255 )
     love.window.setTitle( "Box War" )
 
     w_width, w_height = love.window.getDimensions()
 
+    -- Load graphic stuff
     background = love.graphics.newImage( "graphics/grass1.png" )
-    da_box = love.graphics.newImage( "graphics/chest.png" )
+    g_box = love.graphics.newImage( "graphics/chest.png" )
     g_thief = love.graphics.newImage( "graphics/thief.png" )
 
-    BOX_SIZE = da_box:getWidth()
+    BOX_SIZE = g_box:getWidth()
 
     -- Randomly draw box
     x_box = love.math.random( (w_width - BOX_SIZE - PADDING_AREA) )
     y_box = love.math.random( PADDING_AREA, (w_height - BOX_SIZE) )
 end
 
+-----------------------
+-- Main logic update
+-----------------------
 function love.update(dt)
     create_ennemy()
-    move_ennemies()
+    move_ennemies(dt)
     TEsound.cleanup()
 end
 
+-----------------------
+-- Main draw/graphic update
+-----------------------
 function love.draw()
     render_background()
     render_hud()
@@ -41,6 +51,9 @@ function love.draw()
     render_thieves()
 end
 
+-----------------------
+-- Keyboard events
+-----------------------
 function love.keypressed(key)
     if key == "e" then
         for i = 1, table.getn(ennemies), 1 do
@@ -65,25 +78,19 @@ end
 function render_thieves()
     for i = 1, table.getn(ennemies), 1 do
         local thief = ennemies[i]
-        love.graphics.draw( g_thief, thief.x, thief.y, thief.orientation, 0.3, 0.3 )
+        love.graphics.draw( g_thief, thief.x, thief.y, thief.orientation, 0.3, 0.3, g_thief:getWidth()/2, g_thief:getHeight()/2 )
     end
 end
 
-function move_ennemies()
+function move_ennemies(dt)
     for i = 1, table.getn(ennemies), 1 do
         local thief = ennemies[i]
-
-        if(thief.x == x_box and thief.y == y_box) then goto continue end
-
-        thief.x = thief.x + (0.1 * thief.speed)
-        thief.y = thief.y + (0.1 * thief.speed)
-
-        ::continue::
+        thief:update_position(dt)
     end
 end
 
 function draw_box(x, y)
-    love.graphics.draw( da_box, x, y )
+    love.graphics.draw( g_box, x, y, 0, 1, 1, g_box:getWidth()/2, g_box:getHeight()/2 )
 end
 
 function create_ennemy()
@@ -97,9 +104,30 @@ function create_ennemy()
     if( diff > apparation_cooldown ) then
         TEsound.play("sound_fx/thief_appears.wav")
 
-        thief = Thief:new(ennemy_id, love.math.random(10))
-        thief.x = love.math.random(50)
-        thief.y = love.math.random(50)
+        thief = Thief:new(ennemy_id, love.math.random(50, 100))
+
+        -- Apparation zone
+        rand_zone = love.math.random(1, 4)
+        birth_x = 0
+        birth_y = 0
+
+        if rand_zone == 1 then          -- North
+            birth_y = 0
+            birth_x = love.math.random(w_width)
+        elseif rand_zone == 2 then     -- South
+            birth_y = w_height
+            birth_x = love.math.random(w_width)
+        elseif rand_zone == 3 then     -- East
+            birth_x = 0
+            birth_y = love.math.random(w_height)
+        else                            -- West
+            birth_x = w_width
+            birth_y = love.math.random(w_height)
+        end
+
+        thief.x = birth_x
+        thief.y = birth_y
+        thief:set_destination(x_box, y_box)
 
         table.insert(ennemies, thief)
 
--- thief.lua
+++ thief.lua
@@ -5,15 +5,66 @@ Thief = class('Thief')
 function Thief:initialize(id, speed, x, y)
     self.id = id
     self.speed = speed
-    self.x = x
-    self.y = y
+    self.destination_x = nil
+    self.destination_y = nil
     self.orientation = 0
-end
-
-function set_x(x)
     self.x = x
+    self.y = y
 end
 
-function set_y(y)
-    self.y = y
+function Thief:set_destination(x, y)
+    self.destination_x = x
+    self.destination_y = y
+end
+
+function Thief:update_position(dt)
+    if(self.x == self.destination_x and self.y == self.destination_y) then goto continue end
+
+    x_diff = self.destination_x - self.x
+    y_diff = self.destination_y - self.y
+
+    if x_diff > 0 then
+        new_x = self.x + (dt * self.speed)
+        if new_x > self.destination_x then new_x = self.destination_x end
+    else
+        new_x = self.x - (dt * self.speed)
+        if new_x < self.destination_x then new_x = self.destination_x end
+    end
+
+    self.x = new_x
+
+    if y_diff > 0 then
+        new_y = self.y + (dt * self.speed)
+        if new_y > self.destination_y then new_y = self.destination_y end
+    else
+        new_y = self.y - (dt * self.speed)
+        if new_y < self.destination_y then new_y = self.destination_y end
+    end
+
+    self.y = new_y
+
+    -- Update orientation
+    self:update_orientation(x_diff, y_diff)
+
+    ::continue::
+end
+
+function Thief:update_orientation(x_diff, y_diff)
+    if x_diff > 0 and y_diff < 0 then
+        self.orientation = 7 * math.pi / 4          -- Direction N-E
+    elseif x_diff > 0 and y_diff == 0 then
+        self.orientation = 0                        -- Direction E
+    elseif x_diff > 0 and y_diff > 0 then
+        self.orientation = math.pi / 4              -- Direction S-E
+    elseif x_diff == 0 and y_diff > 0 then
+        self.orientation = math.pi / 2              -- Direction S
+    elseif x_diff < 0 and y_diff > 0 then
+        self.orientation = 3 * math.pi / 4          -- Direction S-W
+    elseif x_diff < 0 and y_diff == 0 then
+        self.orientation = math.pi                  -- Direction W
+    elseif x_diff < 0 and y_diff < 0 then
+        self.orientation =  5 * math.pi / 4         -- Direction N-W
+    elseif x_diff == 0 and y_diff < 0 then
+        self.orientation = 3 * math.pi / 2              -- Direction N
+    end
 end
